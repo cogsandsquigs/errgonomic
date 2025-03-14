366329861318a07800ad6e6011afb4459f5bf0e7
Ian Pratt <ianjdpratt@gmail.com>
2025-03-14 19:30:05 +0000

    moar test

--- src/combinators/consumed.rs
+++ src/combinators/consumed.rs
@@ -20,7 +20,8 @@ pub fn consumed<I: Underlying, O, E: CustomError, P: Parser<I, O, E>>(
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::combinators::is;
+    use crate::combinators::{eoi, is};
+    use crate::parser::errors::{Error, ErrorKind, ExpectedError};
     use crate::parser::Parser;
 
     #[test]
@@ -30,5 +31,24 @@ mod tests {
         assert_eq!(parsed, "te");
         assert_eq!(state.as_input().as_inner(), "st");
         assert!(!state.is_err());
+    }
+
+    #[test]
+    fn can_parse_consumed_eoi() {
+        let (state, parsed): (State<&str>, Input<&str>) = consumed(eoi).process("".into()).unwrap();
+        assert_eq!(parsed, "");
+        assert_eq!(state.as_input().as_inner(), "");
+        assert!(!state.is_err());
+
+        // Makes sure we err if not eoi!
+        let state: State<&str> = consumed(eoi).process("test".into()).unwrap_err();
+        assert!(state.is_err());
+        assert_eq!(
+            state.errors(),
+            &Error::new(
+                ErrorKind::Expected(ExpectedError::Nothing),
+                Input::new_with_span("test", 0..4)
+            )
+        );
     }
 }
